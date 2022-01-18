ae471fc6d94c14b12aa817d56301b6fae73309b8
GersonAparecidoLima <45687389+GersonAparecidoLima@users.noreply.github.com>
2022-01-18 10:22:20 -0300

    Rotas Por Atributo
    Rotas Por Atributo

---
+++ HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    // configurando um prefixo
+    // Não e recomendado colocar RoutePrefix na controller Homme por que ele já e um rota inicial
+    // aqui estamos utilizando para o nosso aprendizagem
+    // aqui estamos chamando Principal
+    //[RoutePrefix("Principal")]
+    // aqui estamos chamado vazio 
+    // por que tudo que estiver vazio, vem para este controller
+    [Route("")]
+    public class HomeController : Controller
+    {
+        // ActionResult e um generico        
+        public ActionResult Index()
+        {
+            return View();
+            // return Content("ops");
+            //return Redirect("http://www.google.com");
+        }
+
+        //retorna um conteudo
+        // caso queira usar ActionResult pode
+        public ContentResult ContentResult() {
+            return Content("ops");
+        }
+
+
+        // caso queira usar ActionResult pode
+        // Nomeando o controller About para 
+        // sobre nos
+        // lembrando que e necessário configurar App_Start\RouteConfig.cs 
+        // com o seguinte comando routes.MapMvcAttributeRoutes();
+        // https:/ /localhost:44377/sobre-nos
+        [Route(template:"sobre-nos" )]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Sua página de descrição do aplicativo.";
+
+            return View();
+            
+        }
+
+        // caso queira usar ActionResult pode
+        //https:/ /localhost:44377/institucional/entre-em-contato
+        [Route(template:"institucional/entre-em-contato")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+// C:\Users\CATALOG\source\repos\WebApplication1\WebApplication1\App_Start\RouteConfig.cs
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //configuração para utilizar uma rota personalizada
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
